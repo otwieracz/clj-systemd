e08c05c92f986ad9509c9a4a69257ff4fc0a29fb
Slawomir Gonet <slawek@otwiera.cz>
2018-08-06 22:31:19 +0200

    travis.yml

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-systemd "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-systemd "0.1.1-SNAPSHOT"
+  :description "Clojure interface to systemd"
+  :url "https://github.com/otwieracz/clj-systemd"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
